74c403b3cc77c0eea0d117af55b1372806623c30
Hakan GÖLGE <hakan.golge@vizyoneks.com.tr>
2025-01-03 00:39:15 +0300

    Initial Commit

---
+++ Lingo Leitner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lingo Leitner
+//
+//  Created by Hakan Gölge on 3.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
